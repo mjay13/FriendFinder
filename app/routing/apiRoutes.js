19c7d2a05ae8ff9d68435aef34f2c0eff66b4aa0
Megan Jacobs <mjay815@hotmail.com>
2017-10-24 16:31:33 -0400

    continuous work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,21 +6,54 @@ var friendList = require("../data/friends.js");
 
 
 module.exports = function(app) {
-	 
-	 // display friend list in json object
-	 app.get('/api/friendlist', function (req, res) {
+
+    // display friend list in json object
+    app.get('/api/friendlist', function(req, res) {
         res.json(friendList);
     });
 
-	//
-	app.post('/api/friends', function(req, res) {
-		
-		// user input object
-		var surveyInput = req.body;
-		
-		// console.log("surveyInput = " + JSON.stringify(surveyInput));
-		friendList.push(surveyInput);
-		res.json(surveyInput);
+    // all logic here
+    app.post('/api/friends', function(req, res) {
 
-		});
+        // user input object
+        var surveyInput = req.body;
+        //console.log(req.body);
+        //console.log(friendList);
+        // console.log("surveyInput = " + JSON.stringify(surveyInput));
+        friendList.push(surveyInput);
+        res.json(surveyInput);
+
+ 
+
+    var bestMatch = {
+        name: "",
+        photoLink: "",
+        answers: [
+            "", // parseInt?
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    };
+
+    for (var i = 0; i < friendList.length; i++) {
+    	//console.log(friendList[i].answers);
+    	var matchAnswers = friendList[i].answers;
+    	console.log("matchAnswers: "+matchAnswers);
+    	
+    	
+    }
+
+   });
+
+
+
+
+
 };
